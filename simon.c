337098512fb4d7acf32741f52022da59aa166927
Chaos90003 <44592761+Chaos90003@users.noreply.github.com>
2025-05-02 11:43:22 -0400

    Current Unfinished state
    -untested simon.c but nothing complicated yet included so I'm not too worried

---
+++ simon.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "simon.h"
+
+//creates the game, will be called in main
+SimonGame* create_game(){
+    SimonGame *game = (SimonGame *)malloc(sizeof(SimonGame));
+    game->length = 0;
+    game->capacity = 4 //for each color - remember to consider making a hard difficulty later
+    game->sequence = (char*)malloc(game->capacity * sizeof(char));
+    srand(time(NULL)); //ensures originality
+    return game;
+
+}
+//checks for space/creates more space and adds the move
+void add_move(SimonGame *game, char move){
+    if (game->length == game->capacity){
+        game->capacity *= 2;
+        char *temp = realloc(game->sequence, game->capacity * sizeof(char));
+        if (temp == NULL){
+            printf("realloc failed");
+        }
+        game->sequence = temp;
+    }
+
+    *(game->sequence + game->length) = move;
+    game->length++;
+
+}
+//prints the sequence of moves
+void print_moves(simonGame *game) {
+    for (char *ptr = game->sequence; ptr < game->sequence + game-> length; ptr++){
+        printf("%c ", *ptr);
+    }
+    printf("\n");
+}
+
+//uses switch and rand() to determine the next move, 
+char move_picker(){
+    switch (rand() % 4){
+        case 0: 
+            return 'R';
+        case 1:
+            return 'G';
+        case 2:
+            return 'B';
+        case 3:
+            return 'Y';
+    }
+}
+
+//compares to see if matches
+//not confident on functionality of this, check first if program fails
+int check_moves(SimonGame *game, char *input){
+   if (memcmp(game->sequence, input, game->length) == 0){
+        return 1; //match
+   }
+   return 0; //don't match
+}
+
+void free_game(SimonGame *game){
+    free(game->sequence);
+    fre(game);
+}
+
+/*
+TODO:
+    -make simple tester in main.c to test what I have
+    -incorporate file i/o by making a high score file
+    -potentially add a harder difficulty incorporating more colors
+    -makefile
+    
+
+
+
+
+*/
---
+++ simon.h
@@ -0,0 +1,18 @@
+#ifndef SIMON_H
+#define SIMON_H
+
+typedef struct {
+    char *sequence;
+    int length;
+    int capacity;
+} SimonGame;
+
+SimonGame* create_game();
+void add_move(SimonGame *game, char move);
+void print_moves(SimonGame *game);
+char move_picker();
+int check_moves(SimonGame *game, char *input);
+void free_game(SimonGame *game);
+
+
+#endif
